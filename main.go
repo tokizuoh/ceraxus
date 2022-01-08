0bb5144bb754cb62db6136b8671ceb0457eca613
tokizuoh <tokizo819@gmail.com>
2022-01-08 17:46:44 +0900

    コマンドライン引数からIDを受け付ける処理を追加

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"model"
 )
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		log.Fatal("コマンドライン引数エラー")
+	}
+
+	storeId := args[0]
+
 	// Fetch
-	url := "https://itunes.apple.com/jp/rss/customerreviews/id=340368403/json"
+	url := fmt.Sprintf("https://itunes.apple.com/jp/rss/customerreviews/id=%s/json", storeId)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -47,8 +58,8 @@ func main() {
 		version := entry.ImVersion.Label
 		name := entry.Author.Name.Label
 		date := entry.Updated.Label
-		title := entry.Title.Label
-		content := entry.Content.Label
+		title := strings.Replace(entry.Title.Label, "\n", "", -1)
+		content := strings.Replace(entry.Content.Label, "\n", "", -1)
 
 		line := []string{id, version, name, date, title, content}
 		if err := w.Write(line); err != nil {
